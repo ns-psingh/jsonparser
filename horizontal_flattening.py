22b90417101cc806e631628350fd8a4e6ad3e71d
Premal <premals98@gmail.com>
2020-06-30 14:26:07 +0530

    Implemented basic feature for flattening arrays and objects

--- horizontal_flattening.py
+++ horizontal_flattening.py
@@ -4,28 +4,56 @@ Module for handling horizontal flatenning
 
 class HorizonalFlattening():
     
-    def _init_(self):
+    def __init__(self):
         self.flatten_objects = False
-        pass
+        self.flatten_arrays = 0
+
+    def flatten_objects_set_value(self, value):
+        self.flatten_objects = value
+    
+    def flatten_objects_get_value(self):
+        return self.flatten_objects
+
+    def flatten_arrays_set_value(self, value):
+        self.flatten_arrays = value
+
+    def flatten_arrays_get_value(self):
+        return self.flatten_arrays
 
     def horizontal_flat(self, document):
         """
         This method will perform horizontal flatenning for a document
         """
         for field in list(document.keys()):
-            if isinstance(document[field], dict):
-                flatenned_results = self.restructure(field, document[field])
+            if isinstance(document[field], dict) and self.flatten_objects_get_value():
+                flatenned_results = self.restructure_flatten_object(field, document[field])
                 del document[field]
                 for result in list(flatenned_results.keys()):
                     document[result] = flatenned_results[result]
+            if isinstance(document[field], list) and self.flatten_arrays_get_value() > 0:
+                flatenned_results = self.restructure_flatten_array(field, document[field], self.flatten_arrays_get_value())
+                for _ in range(0, self.flatten_arrays_get_value()):
+                    del document[field][_]
+                for result in list(flatenned_results.keys()):
+                    document[result] = flatenned_results[result] 
+        return document
 
-    def restructure(self, field_name, document):
+    def restructure_flatten_object(self, field_name, document):
         """
-        This function will take the embedded document and return a dict
+        This function will take the embedded document and return a dict to flatten arrays
         """
         result = {}
         for key in list(document.keys()):
             result[field_name+"."+key] = document[key]
+        return result
+    
+    def restructure_flatten_array(self, field_name, document, num):
+        """
+        This function will take the embedded document and return a dict to flatten objects
+        """
+        result = {}
+        for _ in range(0,num):
+            result[field_name+"."+str(_)] = document[_]
         return result
 
     def display_in_sql_table(self):
--- test_horizontal_flattening.py
+++ test_horizontal_flattening.py
@@ -43,5 +43,26 @@ class TestCovid(unittest.TestCase):
     def setUp(self):
         self.hf = hf.HorizonalFlattening()
 
-    def test_horizontal_flat(self):
-        self.hf.horizontal_flat(sample_document)
+    def test_horizontal_flat_simple(self):
+        assert self.hf.horizontal_flat(sample_document) == sample_document
+    
+    def test_horizontal_flatten_object(self):
+        self.hf.flatten_arrays_set_value(2)
+        self.hf.flatten_objects_set_value(False)
+        assert self.hf.horizontal_flat(sample_document) == {'_id': '5780046cd5a397806c3dab38',
+                                                            'address': {'building': '1007', 
+                                                                        'coord': [-73.856077, 40.848447], 
+                                                                        'street': 'Morris Park Ave', 
+                                                                        'zipcode': '10462'}, 
+                                                            'borough': 'Bronx', 
+                                                            'cuisine': 'Bakery', 
+                                                            'grades': [{'date': '2013-09-11T00:00:00Z', 'grade': 'A', 'score': 6}, 
+                                                                       {'date': '2011-11-23T00:00:00Z', 'grade': 'A', 'score': 9}, 
+                                                                       {'date': '2011-03-10T00:00:00Z', 'grade': 'B', 'score': 14}], 
+                                                            'name': 'Morris Park Bake Shop', 
+                                                            'restaurant_id': '30075445', 
+                                                            'grades.0': {'date': '2014-03-03T00:00:00Z', 'grade': 'A', 'score': 2}, 
+                                                            'grades.1': {'date': '2013-09-11T00:00:00Z', 'grade': 'A', 'score': 6}}
+
+
+    
